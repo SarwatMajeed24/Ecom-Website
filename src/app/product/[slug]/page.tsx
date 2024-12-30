5802450f8587759e7d26ff5c42f1967f84b080ce
SarwatMajeed24 <sarwatmajeed2015@gmail.com>
2024-12-30 11:49:46 +0500

    Fixed build Error

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,17 +1,52 @@
-"use client"
-import { ProductDetails } from "@/app/components"
+// "use client"
+// import { useParams } from "next/navigation";
+// import { ProductDetails } from "@/app/components"
+// import { groq } from "next-sanity";
+// import { client } from "@/sanity/lib/client";
+
+// const Page = async () => {
+//     const { slug }: any  = useParams();
+//     const products = await client.fetch(groq`*[_type=="product"]`)
+//     const product = products.find((product: any)=> product.slug.current == slug)
+//     console.log(product)
+//   return (
+//     <div>
+//         <ProductDetails product={product} />
+//     </div>
+//   )
+// }
+// export default Page
+
+"use client";
 import { useParams } from "next/navigation";
-import { client } from "@/sanity/lib/client";
+import { ProductDetails } from "@/app/components";
+import { useEffect, useState } from "react";
 import { groq } from "next-sanity";
-const Page = async () => {
-    const {slug}: any  = useParams();
-    const products = await client.fetch(groq`*[_type=="product"]`)
-    const product = products.find((product: any)=> product.slug.current == slug)
-    console.log(product)
+import { client } from "@/sanity/lib/client";
+
+const Page = () => {
+  const { slug }: any = useParams();
+  const [product, setProduct] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const products = await client.fetch(groq`*[_type=="product"]`);
+      const foundProduct = products.find((product: any) => product.slug.current === slug);
+      setProduct(foundProduct);
+    };
+
+    fetchProduct();
+  }, [slug]);
+
+  if (!product) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-        <ProductDetails product={product} />
+      <ProductDetails product={product} />
     </div>
-  )
-}
-export default Page
+  );
+};
+
+export default Page;
