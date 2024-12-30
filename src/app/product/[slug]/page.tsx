d3386d453a2196bb3d6ab324e841cad1a05a7ebb
SarwatMajeed24 <sarwatmajeed2015@gmail.com>
2024-12-30 12:13:13 +0500

    Fixed Errors

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,52 +1,52 @@
-// "use client"
-// import { useParams } from "next/navigation";
-// import { ProductDetails } from "@/app/components"
-// import { groq } from "next-sanity";
-// import { client } from "@/sanity/lib/client";
-
-// const Page = async () => {
-//     const { slug }: any  = useParams();
-//     const products = await client.fetch(groq`*[_type=="product"]`)
-//     const product = products.find((product: any)=> product.slug.current == slug)
-//     console.log(product)
-//   return (
-//     <div>
-//         <ProductDetails product={product} />
-//     </div>
-//   )
-// }
-// export default Page
-
-"use client";
+"use client"
 import { useParams } from "next/navigation";
-import { ProductDetails } from "@/app/components";
-import { useEffect, useState } from "react";
+import { ProductDetails } from "@/app/components"
 import { groq } from "next-sanity";
 import { client } from "@/sanity/lib/client";
 
-const Page = () => {
-  const { slug }: any = useParams();
-  const [product, setProduct] = useState<any>(null);
-
-  useEffect(() => {
-    const fetchProduct = async () => {
-      const products = await client.fetch(groq`*[_type=="product"]`);
-      const foundProduct = products.find((product: any) => product.slug.current === slug);
-      setProduct(foundProduct);
-    };
-
-    fetchProduct();
-  }, [slug]);
-
-  if (!product) {
-    return <div>Loading...</div>;
-  }
-
+const Page = async () => {
+    const { slug }: any  = useParams();
+    const products = await client.fetch(groq`*[_type=="product"]`)
+    const product = products.find((product: any)=> product.slug.current == slug)
+    console.log(product)
   return (
     <div>
-      <ProductDetails product={product} />
+        <ProductDetails product={product} />
     </div>
-  );
-};
+  )
+}
+export default Page
 
-export default Page;
+// "use client";
+// import { useParams } from "next/navigation";
+// import { ProductDetails } from "@/app/components";
+// import { useEffect, useState } from "react";
+// import { groq } from "next-sanity";
+// import { client } from "@/sanity/lib/client";
+
+// const Page = () => {
+//   const { slug }: any = useParams();
+//   const [product, setProduct] = useState<any>(null);
+
+//   useEffect(() => {
+//     const fetchProduct = async () => {
+//       const products = await client.fetch(groq`*[_type=="product"]`);
+//       const foundProduct = products.find((product: any) => product.slug.current === slug);
+//       setProduct(foundProduct);
+//     };
+
+//     fetchProduct();
+//   }, [slug]);
+
+//   if (!product) {
+//     return <div>Loading...</div>;
+//   }
+
+//   return (
+//     <div>
+//       <ProductDetails product={product} />
+//     </div>
+//   );
+// };
+
+// export default Page;
