5ec12cfc8d18184f565b41841b32d0a369cc4462
SarwatMajeed24 <sarwatmajeed2015@gmail.com>
2024-12-30 09:31:34 +0500

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,35 @@
-import Image from "next/image";
-import {groq} from "next-sanity"
-import {client} from "@/sanity/lib/client"
-import { Navbar, Hero, Products} from "./components";
+// import Image from "next/image";
+// import {groq} from "next-sanity"
+// import {client} from "@/sanity/lib/client"
+// import { Navbar, Hero, Products} from "./components";
 
-export default async function Home() {
-  const products = await client.fetch(groq`*[_type=="product"]`)
-  console.log(products)
+// export default async function Home() {
+//   const products = await client.fetch(groq`*[_type=="product"]`)
+//   console.log(products)
+//   return (
+//     <>
+//     {/* <Navbar /> */}
+//     <Hero />
+//     <Products />
+//     </>
+//   );
+// }
+
+
+'use client'
+
+import { useState} from 'react';
+import { Hero, Products } from './components';
+import {  AppContext } from "./context/AppContext"
+
+export default function Home() {
+  const [showCart, setShowCart] = useState(false);
+  console.log(showCart);
+
   return (
-    <>
-    {/* <Navbar /> */}
-    <Hero />
-    <Products />
-    </>
-  );
+    <AppContext.Provider value={{ showCart, setShowCart }}>
+      <Hero />
+      <Products />
+    </AppContext.Provider>
+  )
 }
